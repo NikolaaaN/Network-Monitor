8aa73050f564de199a407a1c6b6a98b0c86c3059
Nikola <nikola.neskovic99@gmail.com>
2023-04-05 20:14:18 +0200

    added copyright

--- app/src/main/java/com/example/networkmonitor/MainActivity.java
+++ app/src/main/java/com/example/networkmonitor/MainActivity.java
@@ -1,3 +1,11 @@
+/*
+Copyright (c) 2022, Nikola Nešković
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+*/
+
 package com.example.networkmonitor;
 //nazovi je my internet tracker lite
 
--- app/src/main/java/com/example/networkmonitor/SettingsAdapter.java
+++ app/src/main/java/com/example/networkmonitor/SettingsAdapter.java
@@ -1,3 +1,10 @@
+/*
+Copyright (c) 2022, Nikola Nešković
+All rights reserved.
+
+This source code is licensed under the BSD-style license found in the
+LICENSE file in the root directory of this source tree.
+*/
 package com.example.networkmonitor;
 
 import android.content.Context;
